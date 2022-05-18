dbfc3642ab2321ac97c23423176f924b7ef0d073
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-05-18 10:17:49 +0100

    put time in own namespace

--- src/fmwc/model.clj
+++ src/fmwc/model.clj
@@ -1,10 +1,7 @@
 (ns fmwc.model
   (:require [fmwc.framework2 :as fw]
-            [fmwc.model.utils :refer [equal-to?
-                                      flagged? make-flag
-                                      add-months end-of-month add-days
-                                      month-of year-of
-                                      date> date<=]]))
+            [fmwc.model.time]
+            [fmwc.model.utils :refer :all]))
 
 ;; Inputs
 ;;;;;;;;;;;;;;;;
@@ -24,89 +21,6 @@
     :power-tariff               {:units "$/KWh" :starter 0.065}
     :yields                     {:units "percent" :starter [0.33 0.36 0.155 0.155]}
     :availability               {:units "percent" :starter 0.97}})
-
-;; TIME
-;;;;;;;;;;;;;;;;
-
-(def model-column-counter
-  {:units "counter"
-   :starter 0
-   :calculator '(inc [:prev :self])})
-
-(def first-model-column-flag
-  {:units "flag"
-   :calculator '((equal-to? 1) [:this :time/model-column-counter])})
-
-(def model-period-beginning
-  {:units "date"
-   :calculator
-   '(if (flagged? [:this :time/first-model-column-flag])
-      [:const :inputs/first-date-of-time-rulers]
-      (add-months [:prev :self] [:const :inputs/length-of-operating-period]))})
-
-(def model-period-ending
-  {:units "date"
-   :calculator '(add-days (add-months [:this :time/model-period-beginning]
-                                      [:const :inputs/length-of-operating-period])
-                          -1)})
-
-(def end-of-contract-year-flag
-  {:units "flag"
-   :starter 0
-   :calculator '(make-flag (= (month-of [:const :inputs/aquisition-date])
-                              (month-of [:this :time/model-period-ending])))})
-
-(def end-of-operating-period
-  {:units "date"
-   :calculator '(end-of-month [:const :inputs/aquisition-date]
-                              (* 12 [:const :inputs/operating-years-remaining]))})
-
-(def operating-period-flag
-  {:units "flag"
-   :calculator '(make-flag (and (date> [:this  :time/model-period-ending]
-                                       [:const :inputs/aquisition-date])
-                                (date<= [:this  :time/model-period-ending]
-                                        [:this :time/end-of-operating-period])))})
-
-(def period-number
-  {:units "counter"
-   :starter 0
-   :calculator  '(* (if (= [:prev :self] [:const :inputs/periods-in-year]) 1
-                        (inc [:prev :self]))
-                    [:this :time/operating-period-flag])})
-
-(def contract-year-number
-  {:units "counter"
-   :starter 0
-   :calculator '(* (+ [:prev :self] [:prev :time/end-of-contract-year-flag])
-                   [:this :time/operating-period-flag])})
-
-(def contract-year-applicable-to-period
-  {:units "date"
-   :calculator '(cond (flagged? [:this :time/first-model-column-flag])
-                      [:const :inputs/annual-year-end-date-of-first-operating-period]
-
-                      (flagged? [:prev :time/end-of-contract-year-flag])
-                      (end-of-month [:prev :self] 12)
-                      :else [:prev :self])})
-
-(def contract-year
-  {:units "year"
-   :calculator '(year-of [:this :time/contract-year-applicable-to-period])})
-
-(def time-rows
-  #:time
-   {:model-column-counter            model-column-counter
-    :first-model-column-flag         first-model-column-flag
-    :model-period-beginning          model-period-beginning
-    :model-period-ending             model-period-ending
-    :end-of-contract-year-flag       end-of-contract-year-flag
-    :end-of-operating-period         end-of-operating-period
-    :operating-period-flag           operating-period-flag
-    :period-number                   period-number
-    :contract-year-number            contract-year-number
-    :contract-year-applicable-to-period contract-year-applicable-to-period
-    :contract-year                   contract-year})
 
 (def compound-degradation
   {:units "percent"
@@ -144,7 +58,7 @@
     :electricity-generation         electricity-generation
     :electricity-generation-revenue electricity-generation-revenue})
 
-(def model (fw/make-model [time-rows inputs revenue-rows]))
+(def model (fw/make-model [fmwc.model.time/time-rows inputs revenue-rows]))
 (def deps (fw/dependency-order model))
 (def output (fw/run2 model 5))
 
---
+++ src/fmwc/model/time.clj
@@ -0,0 +1,84 @@
+(ns fmwc.model.time)
+
+;; TIME
+;;;;;;;;;;;;;;;;
+
+(def model-column-counter
+  {:units "counter"
+   :starter 0
+   :calculator '(inc [:prev :self])})
+
+(def first-model-column-flag
+  {:units "flag"
+   :calculator '((equal-to? 1) [:this :time/model-column-counter])})
+
+(def model-period-beginning
+  {:units "date"
+   :calculator
+   '(if (flagged? [:this :time/first-model-column-flag])
+      [:const :inputs/first-date-of-time-rulers]
+      (add-months [:prev :self] [:const :inputs/length-of-operating-period]))})
+
+(def model-period-ending
+  {:units "date"
+   :calculator '(add-days (add-months [:this :time/model-period-beginning]
+                                      [:const :inputs/length-of-operating-period])
+                          -1)})
+
+(def end-of-contract-year-flag
+  {:units "flag"
+   :starter 0
+   :calculator '(make-flag (= (month-of [:const :inputs/aquisition-date])
+                              (month-of [:this :time/model-period-ending])))})
+
+(def end-of-operating-period
+  {:units "date"
+   :calculator '(end-of-month [:const :inputs/aquisition-date]
+                              (* 12 [:const :inputs/operating-years-remaining]))})
+
+(def operating-period-flag
+  {:units "flag"
+   :calculator '(make-flag (and (date> [:this  :time/model-period-ending]
+                                       [:const :inputs/aquisition-date])
+                                (date<= [:this  :time/model-period-ending]
+                                        [:this :time/end-of-operating-period])))})
+
+(def period-number
+  {:units "counter"
+   :starter 0
+   :calculator  '(* (if (= [:prev :self] [:const :inputs/periods-in-year]) 1
+                        (inc [:prev :self]))
+                    [:this :time/operating-period-flag])})
+
+(def contract-year-number
+  {:units "counter"
+   :starter 0
+   :calculator '(* (+ [:prev :self] [:prev :time/end-of-contract-year-flag])
+                   [:this :time/operating-period-flag])})
+
+(def contract-year-applicable-to-period
+  {:units "date"
+   :calculator '(cond (flagged? [:this :time/first-model-column-flag])
+                      [:const :inputs/annual-year-end-date-of-first-operating-period]
+
+                      (flagged? [:prev :time/end-of-contract-year-flag])
+                      (end-of-month [:prev :self] 12)
+                      :else [:prev :self])})
+
+(def contract-year
+  {:units "year"
+   :calculator '(year-of [:this :time/contract-year-applicable-to-period])})
+
+(def time-rows
+  #:time
+   {:model-column-counter            model-column-counter
+    :first-model-column-flag         first-model-column-flag
+    :model-period-beginning          model-period-beginning
+    :model-period-ending             model-period-ending
+    :end-of-contract-year-flag       end-of-contract-year-flag
+    :end-of-operating-period         end-of-operating-period
+    :operating-period-flag           operating-period-flag
+    :period-number                   period-number
+    :contract-year-number            contract-year-number
+    :contract-year-applicable-to-period contract-year-applicable-to-period
+    :contract-year                   contract-year})
