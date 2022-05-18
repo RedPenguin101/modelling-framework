1f0a9c5aff2130f549b097667d8e6ab432ae67ec
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-05-18 11:58:05 +0100

    rename fns in utils

--- src/fmwc/model/utils.clj
+++ src/fmwc/model/utils.clj
@@ -4,30 +4,30 @@
 (defn equal-to? [x] (fn [val] (if (= x val) 1 0)))
 (defn flagged? [x] (not (zero? x)))
 
-(defn str-to-date [date-str] (java.time.LocalDate/parse date-str))
-(defn date-to-str [date] (.toString date))
+(defn str->date [date-str] (java.time.LocalDate/parse date-str))
+(defn date->str [date] (.toString date))
 
-(defn date-comp [a b] (.compareTo (str-to-date a) (str-to-date b)))
+(defn date-comp [a b] (.compareTo (str->date a) (str->date b)))
 (defn date< [a b] (neg? (date-comp a b)))
 (defn date= [a b] (zero? (date-comp a b)))
 (defn date> [a b] (pos? (date-comp a b)))
 (defn date<= [a b] (or (date< a b) (date= a b)))
 (defn date>= [a b] (or (date> a b) (date= a b)))
-(defn month-of [d] (.getMonthValue (str-to-date d)))
-(defn year-of [d] (.getYear (str-to-date d)))
+(defn month-of [d] (.getMonthValue (str->date d)))
+(defn year-of [d] (.getYear (str->date d)))
 
 (defn add-months [date months]
   (when date
-    (date-to-str (.plusMonths (str-to-date date) months))))
+    (date->str (.plusMonths (str->date date) months))))
 
 (defn add-days [date days]
   (when date
-    (date-to-str (.plusDays (str-to-date date) days))))
+    (date->str (.plusDays (str->date date) days))))
 
 (defn end-of-month [date months]
-  (let [month-adj (.plusMonths (str-to-date date) months)]
-    (date-to-str (.plusDays month-adj (- (.lengthOfMonth month-adj)
-                                         (.getDayOfMonth month-adj))))))
+  (let [month-adj (.plusMonths (str->date date) months)]
+    (date->str (.plusDays month-adj (- (.lengthOfMonth month-adj)
+                                       (.getDayOfMonth month-adj))))))
 
 (comment
   (end-of-month "2021-03-30" (* 12 25))
