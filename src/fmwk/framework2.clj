c6d7d4fdf814dfabbb9da8709b79c4d43ffe664d
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-05-21 12:19:40 +0100

    start of redo

---
+++ src/fmwk/framework2.clj
@@ -0,0 +1,138 @@
+(ns fmwk.framework2
+  (:require [clojure.walk :refer [postwalk]]
+            [clojure.spec.alpha :as spec]
+            [ubergraph.core :as uber]
+            [ubergraph.alg :as uberalg]))
+
+;; utils
+;;;;;;;;;;;;;;
+
+(defn map-vals [f m] (update-vals m f))
+
+(defn qualify [qualifier-str kw]
+  (if (or (= :placeholder kw) (qualified-keyword? kw)) kw
+      (keyword qualifier-str (name kw))))
+
+(defn- expand
+  "Where the value in a kv pair is a sequence, this function
+   will 'unpack' the value. So 
+     [:a [:b :c :d]]
+   will become
+     [[:a :b] [:a :c] [:a :d]]"
+  [[k vs]] (map #(vector k %) vs))
+
+(comment
+  (qualify "hello.world" :foo)
+  (qualify "hello.world" :other.ns/foo))
+
+;; References and calculation expressions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(spec/def :framework.reference/current (spec/tuple keyword?))
+(spec/def :framework.reference/previous (spec/tuple keyword? #(= :prev %)))
+(spec/def :framework.reference/placeholder (spec/tuple #(= :placeholder %) any?))
+
+(defn placeholder-ref? [ref] (= :placeholder (first ref)))
+(defn previous-period-ref? [ref] (= :prev (second ref)))
+(defn current-period-ref? [ref] (= 1 (count ref)))
+(defn constant? [ref] (#{:placeholder :constant} (first ref)))
+
+(spec/valid? :framework.reference/current [:hello])
+(spec/valid? :framework.reference/previous [:hello :prev])
+(spec/valid? :framework.reference/placeholder [:placeholder 6])
+
+(defn extract-refs
+  "Given an expression containing references (defined as a vector), 
+   will extract all of the references and return them as a vector"
+  ([expr] (if (coll? expr)
+            (extract-refs [] expr)
+            (throw (ex-info "extract-refs: expression is a constant" {:expr expr}))))
+  ([found [fst & rst :as expr]]
+   (cond (vector? expr) (conj found expr)
+         (nil? fst) found
+         (vector? fst) (recur (conj found fst) rst)
+         (coll? fst) (recur (into found (extract-refs [] fst)) rst)
+         :else (recur found rst))))
+
+(defn replace-local-references [qualifier expr]
+  (postwalk #(if (vector? %) (update % 0 (partial qualify qualifier)) %)
+            expr))
+
+(defn replace-refs-in-expr [expr replacements]
+  (postwalk #(cond (and (vector? %) (= :placeholder (first %))) (second %)
+                   (vector? %) (replacements (first %))
+                   :else %)
+            expr))
+
+;; Row and calculation helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn inputs->rows [inputs]
+  (update-vals inputs :value))
+
+
+
+;; calculations helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Calc validations
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn multiple-placeholders-in-expr? [expr]
+  (> (count (filter #{:placeholder} (map first (extract-refs expr))))
+     1))
+
+(multiple-placeholders-in-expr? '(+ [:placeholder 10] [:placeholder 10]))
+
+;; model helpers
+;;;;;;;;;;;;;;;;;;;;;
+
+;; model validations
+;;;;;;;;;;;;;;;;;;;;;
+
+;; Model building
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Model dependecies
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn calculate-order [rows]
+  (->> (map-vals extract-refs rows)
+       (map-vals #(filter current-period-ref? %))
+       (map-vals #(map first %))
+       (mapcat expand)
+       (uber/add-directed-edges* (uber/digraph))
+       (uberalg/topsort)
+       reverse))
+
+(calculate-order {:a '(inc [:c])
+                  :b '(+ [:b :prev]
+                         [:a])
+                  :c [:placeholder 4]})
+
+;; Model running
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn zero-period
+  [rows]
+  (update-vals rows #(if (coll? %) 0 %)))
+
+(defn resolve-reference [ref this-record [previous-record]]
+  (cond (constant? ref)            (second ref)
+        (previous-period-ref? ref) (get previous-record (first ref))
+        (current-period-ref? ref)  (get this-record (first ref))))
+
+(defn next-period [prv-recs rows calc-order]
+  (reduce (fn [record row-name]
+            (let [refs (extract-refs (row-name rows))]
+              (println refs)
+              (->> (map #(resolve-reference % record prv-recs) refs)
+                   (zipmap (map first refs))
+                   (replace-refs-in-expr (row-name rows))
+                   eval
+                   (assoc record row-name))))
+          {}
+          calc-order))
+
+;; Table printing and model selection
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
---
+++ test/fmwk_test/fmwk_test.clj
@@ -0,0 +1,270 @@
+(ns fmwk-test.fmwk-test
+  (:require [clojure.test :refer [deftest is testing are]]
+            [clojure.spec.alpha :as spec]
+            [fmwk.utils :refer :all]
+            [fmwk.framework2 :as SUT]))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Testing Model
+;;;;;;;;;;;;;;;;;;;;
+
+;; Time
+;;;;;;;;;;;;;;;;;;;;
+
+(def time-calcs
+  #:time
+   {:model-column-number '(inc [:model-column-number :prev])
+    :first-model-column-flag '(if (= 1 [:model-column-number]) 1 0)
+    :period-start-date '(if (= 1 [:inputs/first-model-column-flag])
+                          [:inputs/model-start-date]
+                          (add-days [:period-end-date :prev] 1))
+    :period-end-date '(add-days (add-months [:period-start-date]
+                                            [:inputs/length-of-operating-period])
+                                -1)
+    :financial-close-period-flag '(and (date>= [:inputs/aquisition-date]
+                                               [:period-start-date])
+                                       (date<= [:inputs/aquisition-date]
+                                               [:period-end-date]))
+    :financial-exit-period-flag '(and (date>= [:inputs/end-of-operating-period]
+                                              [:period-start-date])
+                                      (date<= [:inputs/end-of-operating-period]
+                                              [:period-end-date]))
+    :end-of-operating-period '(end-of-month [:inputs/aquisition-date]
+                                            (* 12 [:inputs/operating-years-remaining]))
+    :operating-period-flag '(and (date> [:period-start-date]
+                                        [:inputs/aquisition-date])
+                                 (date<= [:period-end-date]
+                                         [:inputs/end-of-operating-period]))})
+
+;; Inputs
+;;;;;;;;;;;;;;;;;;;;
+
+(def inputs
+  #:inputs
+   {:model-start-date           {:value "2020-01-01"}
+    :inflation-rate             {:value 1.02}
+    :length-of-operating-period {:value 12}
+    :aquisition-date            {:value "2020-12-31"}
+    :operating-years-remaining  {:value 15}
+    :sale-date                  {:value "2035-12-31"}
+    :starting-price             {:value 50.0}
+    :starting-costs             {:value (+ 4.75 1.5)}
+    :starting-tax               {:value 1250}
+    :purchase-price             {:value 2000000.0}
+    :volume-at-aquisition       {:value 50000.0}
+    :management-fee-rate        {:value 0.015}
+    :ltv                        {:value 0.6}
+    :growth-rate                {:value 0.05}
+    :interest-rate              {:value 0.03}
+    :disposition-fee-rate       {:value 0.01}
+    :origination-fee-rate       {:value 0.01}})
+
+;; Model
+;;;;;;;;;;;;;;;;;;;;
+
+(def prices
+  #:prices{:inflation-period '(dec [:time/model-column-number])
+           :compound-inflation '(Math/pow
+                                 [:inputs/inflation-rate]
+                                 [:inflation-period])
+           :sale-price '(* [:compound-inflation]
+                           [:inputs/starting-price])
+           :costs {:calculator '(* [:compound-inflation]
+                                   [:inputs/starting-price])}
+           :profit '(- [:sale-price] [:costs])})
+
+(def expenses
+  #:expenses{:tax '(if [:operating-period-flag]
+                     (* [:compound-inflation]
+                        [:starting-tax])
+                     0)
+             :interest '(* [:interest-rate]
+                           [:starting-debt])
+             :management-fee '(if [:operating-period-flag]
+                                (* [:ending-value :prev]
+                                   [:management-fee-rate])
+                                0)
+             :expenses '(+ [:management-fee]
+                           [:tax] [:interest])})
+
+(def closing
+  #:capital.closing
+   {:aquisition-cashflow '(if [:financial-close-period-flag]
+                            [:purchase-price]
+                            0.0)
+    :debt-drawdown '(if [:financial-close-period-flag]
+                      (* [:ltv] [:ending-value])
+                      0)
+    :origination-fee '(if [:financial-close-period-flag]
+                        (* [:origination-fee-rate] [:debt-drawdown])
+                        0)
+    :closing-cashflow '(- [:debt-drawdown]
+                          [:origination-fee]
+                          [:aquisition-cashflow])})
+
+
+(def debt
+  #:debt.debt-balance
+   {:starting-debt  [:ending-debt :prev]
+    :debt-increases [:debt-drawdown]
+    :debt-decreases [:loan-repayment]
+    :ending-debt    '(- (+ [:starting-debt]
+                           [:debt-increases])
+                        [:debt-decreases])})
+
+(def volume
+  #:volume.volume
+   {:starting-volume [:ending-volume :prev]
+    :growth '(* [:starting-volume]
+                [:growth-rate])
+    :harvest '(if (and [:operating-period-flag]
+                       (not [:financial-exit-period-flag]))
+                (/ [:expenses] [:profit])
+                0)
+    :ending-volume '(if [:financial-close-period-flag]
+                      [:volume-at-aquisition]
+                      (- (+ [:starting-volume]
+                            [:growth])
+                         [:harvest]))
+    :ending-value '(* [:ending-volume]
+                      [:profit])})
+
+(def exit
+  #:capital.exit
+   {:sale-proceeds '(if [:financial-exit-period-flag]
+                      [:ending-value]
+                      0)
+    :disposition-fee '(if [:financial-exit-period-flag]
+                        (* [:sale-proceeds] [:disposition-fee-rate])
+                        0)
+    :loan-repayment '(if [:financial-exit-period-flag]
+                       [:starting-debt]
+                       0)
+    :exit-cashflow '(- [:sale-proceeds] [:disposition-fee] [:loan-repayment])})
+
+(def cashflows
+  #:financial-statements.cashflows
+   {:aquisition [:closing-cashflow]
+    :disposition [:exit-cashflow]
+    :gross-profit '(* [:profit] [:harvest])
+    :expenses-paid '(- [:expenses])
+    :net-cashflow '(+ [:closing-cashflow]
+                      [:disposition]
+                      [:gross-profit]
+                      [:expenses-paid])})
+
+(def calculations (merge time-calcs
+                         prices
+                         (SUT/inputs->rows inputs)))
+
+(deftest input-test
+  (is (= (SUT/inputs->rows inputs)
+         #:inputs{:disposition-fee-rate 0.01,
+                  :inflation-rate 1.02,
+                  :purchase-price 2000000.0,
+                  :starting-costs 6.25,
+                  :aquisition-date "2020-12-31",
+                  :sale-date "2035-12-31",
+                  :starting-price 50.0,
+                  :volume-at-aquisition 50000.0,
+                  :model-start-date "2020-01-01",
+                  :growth-rate 0.05,
+                  :interest-rate 0.03,
+                  :management-fee-rate 0.015,
+                  :operating-years-remaining 15,
+                  :starting-tax 1250,
+                  :ltv 0.6,
+                  :length-of-operating-period 12,
+                  :origination-fee-rate 0.01})))
+
+(deftest zero-period-test
+  (is (= (SUT/zero-period calculations)
+         {:prices/profit 0,
+          :time/period-end-date 0,
+          :time/financial-close-period-flag 0,
+          :time/first-model-column-flag 0,
+          :time/operating-period-flag 0,
+          :inputs/disposition-fee-rate 0.01,
+          :inputs/inflation-rate 1.02,
+          :inputs/purchase-price 2000000.0,
+          :time/model-column-number 0,
+          :prices/inflation-period 0,
+          :inputs/starting-costs 6.25,
+          :inputs/aquisition-date "2020-12-31",
+          :prices/sale-price 0,
+          :inputs/sale-date "2035-12-31",
+          :time/financial-exit-period-flag 0,
+          :inputs/starting-price 50.0,
+          :inputs/volume-at-aquisition 50000.0,
+          :inputs/model-start-date "2020-01-01",
+          :prices/costs 0,
+          :inputs/growth-rate 0.05,
+          :inputs/interest-rate 0.03,
+          :inputs/management-fee-rate 0.015,
+          :prices/compound-inflation 0,
+          :inputs/operating-years-remaining 15,
+          :inputs/starting-tax 1250,
+          :inputs/ltv 0.6,
+          :inputs/length-of-operating-period 12,
+          :time/end-of-operating-period 0,
+          :time/period-start-date 0,
+          :inputs/origination-fee-rate 0.01})))
+
+(deftest reference-extractions
+  (is (= (SUT/extract-refs '(* [:compound-inflation]
+                               [:inputs/starting-price]))
+         [[:compound-inflation]
+          [:inputs/starting-price]]))
+
+  (is (= (SUT/extract-refs '(if [:financial-close-period-flag]
+                              (* [:ltv] [:ending-value :prev])
+                              0))
+         [[:financial-close-period-flag] [:ltv] [:ending-value :prev]]))
+
+  (is (thrown-with-msg? Exception
+                        #"extract-refs: expression is a constant"
+                        (SUT/extract-refs 123))))
+
+(deftest replacing-locals
+
+  (is (= (SUT/replace-local-references "hello" [:placeholder 7])
+         [:placeholder 7]))
+  (is (= (SUT/replace-local-references "hello" [:test 7])
+         [:hello/test 7]))
+  (is (= (SUT/replace-local-references "hello" [:already.qualled/test 7])
+         [:already.qualled/test 7]))
+  (is (= (SUT/replace-local-references "hello" 25)
+         25))
+  (is (= (SUT/replace-local-references "hello"
+                                       '(Math/pow [:inputs/inflation-rate] [:inflation-period] [:placeholder 7]))
+         '(Math/pow [:inputs/inflation-rate] [:hello/inflation-period] [:placeholder 7]))))
+
+(deftest replacing-references
+  (is (= (SUT/replace-refs-in-expr [:hello] {:hello 5})
+         5))
+  (is (= (SUT/replace-refs-in-expr [:placeholder 10] {:hello 5})
+         10))
+  (is (= (SUT/replace-refs-in-expr '(+ [:foo] [:bar]) {:foo 5 :bar 10})
+         '(+ 5 10))))
+
+(deftest order-calc-test
+  (is (= (SUT/calculate-order {:a '(inc [:c])
+                               :b '(+ [:b :prev]
+                                      [:a])
+                               :c [:placeholder 4]})
+         [:c :a :b])))
+
+(deftest reference-resolution
+  (is (= (SUT/resolve-reference [:b] {:b 6} [{}]) 6))
+  (is (= (SUT/resolve-reference [:placeholder 7] {} [{}]) 7))
+  (is (= (SUT/resolve-reference [:b :prev] {:b 0} [{:b 8}]) 8)))
+
+(deftest next-period-test
+  (is (= (SUT/next-period [{:a 1 :b 2 :c 3}]
+                          {:a [:placeholder 4]
+                           :b '(inc [:a])
+                           :c '(+ [:c :prev]
+                                  [:b])}
+                          [:a :b :c])
+         {:a 4, :b 5, :c 8})))
+
