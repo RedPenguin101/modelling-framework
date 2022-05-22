9e606dd93ec2aeff3c49983067f6630d764d6097
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-05-22 19:33:13 +0100

    gridlines model up to o&m expense

---
+++ src/fmwk/gridlines/model.clj
@@ -0,0 +1,125 @@
+(ns fmwk.gridlines.model
+  (:require [fmwk.framework2 :as fw]
+            [fmwk.utils :refer :all]))
+
+(def inputs
+  #:inputs
+   {:model-start-date           "2020-07-01"
+    :length-of-operating-period 3
+    :operating-years-remaining  25
+    :aquisition-date            "2021-03-31"
+    :periods-in-year            4
+    :annual-degradation         0.005
+    :seasonal-adjustments-q1    0.33
+    :seasonal-adjustments-q2    0.36
+    :seasonal-adjustments-q3    0.155
+    :seasonal-adjustments-q4    0.155
+    :year1-p50-yield            250
+    :availability               0.97
+    :power-tariff               0.065
+    :annual-real-om-cost        1500})
+
+(def time-calcs
+  (merge
+   #:time.period{:start-date '(if (= 1 [:flags/first-model-column])
+                                [:inputs/model-start-date]
+                                (add-days [:end-date :prev] 1))
+                 :end-date   '(add-days (add-months [:start-date]
+                                                    [:inputs/length-of-operating-period])
+                                        -1)}
+   #:time
+    {:model-column-number     '(inc [:model-column-number :prev])
+     :end-of-operating-period '(end-of-month [:inputs/aquisition-date]
+                                             (* 12 [:inputs/operating-years-remaining]))}
+
+   #:time.contract-year {:end-month      '(month-of [:inputs/aquisition-date])
+                         :year-end-flag  '(= (month-of [:time.period/end-date]) [:end-month])
+                         :number         '(if (and [:flags/operating-period] [:year-end-flag :prev])
+                                            (inc [:number :prev])
+                                            [:number :prev])
+                         :quarter-number '(if [:flags/operating-period]
+                                            (inc (mod [:quarter-number :prev] 4))
+                                            0)}))
+
+(def flags
+  #:flags{:first-model-column '(if (= 1 [:time/model-column-number]) 1 0)
+          :financial-close-period '(and (date>= [:inputs/aquisition-date]
+                                                [:time.period/start-date])
+                                        (date<= [:inputs/aquisition-date]
+                                                [:time.period/end-date]))
+          :financial-exit-period '(and (date>= [:time/end-of-operating-period]
+                                               [:time.period/start-date])
+                                       (date<= [:time/end-of-operating-period]
+                                               [:time.period/end-date]))
+          :operating-period '(and (date> [:time.period/start-date]
+                                         [:inputs/aquisition-date])
+                                  (date<= [:time.period/end-date]
+                                          [:time/end-of-operating-period]))})
+
+(def revenue
+  #:revenue{:compound-degradation '(when-flag [:flags/operating-period]
+                                              (/ 1 (Math/pow (inc [:inputs/annual-degradation]) (dec [:time.contract-year/number]))))
+            :seasonality-adjustment '(case [:time.contract-year/quarter-number]
+                                       1 [:inputs/seasonal-adjustments-q1]
+                                       2 [:inputs/seasonal-adjustments-q2]
+                                       3 [:inputs/seasonal-adjustments-q3]
+                                       4 [:inputs/seasonal-adjustments-q4]
+                                       0)
+            :electricity-generation '(* [:inputs/year1-p50-yield]
+                                        [:inputs/availability]
+                                        [:compound-degradation]
+                                        [:seasonality-adjustment])
+            :revenue-from-generation '(* 1000
+                                         [:electricity-generation]
+                                         [:inputs/power-tariff])})
+
+(def costs
+  #:om-costs{; TODO: Return when escalation factor done
+             ; TODOL Put this on FS
+             :escalation-factor     [:placeholder 1]
+             :expense               '(when-flag [:flags/operating-period]
+                                                (* [:escalation-factor]
+                                                   (/ [:inputs/annual-real-om-cost]
+                                                      [:inputs/periods-in-year])))})
+
+(def fs
+  (merge (fw/add-total :net-cashflow #:fs.cashflow{:cash-from-invoices [:revenue/revenue-from-generation]
+                                                   :dividends-paid [:placeholder 0]})
+
+         (fw/add-total :profit-after-tax #:fs.income{:revenue [:revenue/revenue-from-generation]})
+
+         (fw/add-total :total-assets #:fs.balance-sheet.assets{:retained-cash [:equity.retained-cash/end]
+                                                               :accounts-receivable [:placeholder 0]})
+         (fw/add-total :total-liabilities #:fs.balance-sheet.liabilities{:retained-earnings [:equity.retained-earnings/end]})
+         {:fs.balance-sheet/balance-check '(- [:fs.balance-sheet.assets/total-assets] [:fs.balance-sheet.liabilities/total-liabilities])}))
+
+(def equity
+  (merge (fw/corkscrew "equity.retained-earnings"
+                       [:fs.income/profit-after-tax]
+                       [:fs.cashflow/dividends-paid])
+         (fw/corkscrew "equity.retained-cash"
+                       [:fs.cashflow/net-cashflow]
+                       [:fs.cashflow/dividends-paid])))
+
+;; Build and run
+
+(def model (fw/build-and-validate-model
+            inputs
+            [time-calcs flags
+             revenue costs
+             fs equity]))
+
+(fw/fail-catch (fw/build-and-validate-model
+                inputs
+                [time-calcs flags
+                 fs equity]))
+
+(def results (time (fw/run-model model 10)))
+
+(fw/fail-catch (fw/run-model model 10))
+
+(defn print-calcs [results qualifiers period-range]
+  (doseq [q qualifiers]
+    (fw/slice-results results q period-range)))
+
+(print-calcs results ["om-costs"] [1 10])
