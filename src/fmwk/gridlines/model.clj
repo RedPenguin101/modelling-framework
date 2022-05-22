bc494f3877045f27ed9c1e5a3903e6bf5c9b4b6d
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-05-22 22:47:41 +0100

    model formatting

--- src/fmwk/gridlines/model.clj
+++ src/fmwk/gridlines/model.clj
@@ -24,134 +24,177 @@
 
 (def time-calcs
   (merge
-   #:time.period{:start-date '(if (true? [:flags/first-model-column])
-                                [:inputs/model-start-date]
-                                (add-days [:end-date :prev] 1))
-                 :end-date   '(add-days (add-months [:start-date]
-                                                    [:inputs/length-of-operating-period])
-                                        -1)}
+   #:time.period
+    {:start-date              '(if (true? [:flags/first-model-column])
+                                 [:inputs/model-start-date]
+                                 (add-days [:end-date :prev] 1))
+     :end-date                '(add-days (add-months [:start-date]
+                                                     [:inputs/length-of-operating-period])
+                                         -1)}
    #:time
     {:model-column-number     '(inc [:model-column-number :prev])
      :end-of-operating-period '(end-of-month [:inputs/aquisition-date]
                                              (* 12 [:inputs/operating-years-remaining]))}
 
-   #:time.contract-year {:end-month      '(month-of [:inputs/aquisition-date])
-                         :year-end-flag  '(= (month-of [:time.period/end-date]) [:end-month])
-                         :number         '(if (and [:flags/operating-period] [:year-end-flag :prev])
-                                            (inc [:number :prev])
-                                            [:number :prev])
-                         :quarter-number '(if [:flags/operating-period]
-                                            (inc (mod [:quarter-number :prev] 4))
-                                            0)}))
+   #:time.contract-year
+    {:end-month               '(month-of [:inputs/aquisition-date])
+     :year-end-flag           '(= (month-of [:time.period/end-date]) [:end-month])
+     :number                  '(if (and [:flags/operating-period] [:year-end-flag :prev])
+                                 (inc [:number :prev])
+                                 [:number :prev])
+     :quarter-number          '(if [:flags/operating-period]
+                                 (inc (mod [:quarter-number :prev] 4))
+                                 0)}))
 
 (def flags
-  #:flags{:first-model-column '(= 1 [:time/model-column-number])
-          :financial-close-period '(and (date>= [:inputs/aquisition-date]
-                                                [:time.period/start-date])
-                                        (date<= [:inputs/aquisition-date]
-                                                [:time.period/end-date]))
-          :financial-exit-period '(and (date>= [:time/end-of-operating-period]
-                                               [:time.period/start-date])
-                                       (date<= [:time/end-of-operating-period]
-                                               [:time.period/end-date]))
-          :operating-period '(and (date> [:time.period/start-date]
-                                         [:inputs/aquisition-date])
-                                  (date<= [:time.period/end-date]
-                                          [:time/end-of-operating-period]))
-          :first-operating-period  '(date= [:inputs/aquisition-date]
-                                           (add-days [:time.period/start-date] -1))
-          :last-operating-period  '(date= [:time/end-of-operating-period]
-                                          [:time.period/end-date])})
+  #:flags
+   {:first-model-column       '(= 1 [:time/model-column-number])
+    :financial-close-period   '(and (date>= [:inputs/aquisition-date]
+                                            [:time.period/start-date])
+                                    (date<= [:inputs/aquisition-date]
+                                            [:time.period/end-date]))
+    :financial-exit-period    '(and (date>= [:time/end-of-operating-period]
+                                            [:time.period/start-date])
+                                    (date<= [:time/end-of-operating-period]
+                                            [:time.period/end-date]))
+    :operating-period         '(and (date> [:time.period/start-date]
+                                           [:inputs/aquisition-date])
+                                    (date<= [:time.period/end-date]
+                                            [:time/end-of-operating-period]))
+    :first-operating-period   '(date= [:inputs/aquisition-date]
+                                      (add-days [:time.period/start-date] -1))
+    :last-operating-period    '(date= [:time/end-of-operating-period]
+                                      [:time.period/end-date])})
 
 (def revenue
-  #:revenue{:compound-degradation '(when-flag [:flags/operating-period]
-                                              (/ 1 (Math/pow (inc [:inputs/annual-degradation]) (dec [:time.contract-year/number]))))
-            :seasonality-adjustment '(case [:time.contract-year/quarter-number]
-                                       1 [:inputs/seasonal-adjustments-q1]
-                                       2 [:inputs/seasonal-adjustments-q2]
-                                       3 [:inputs/seasonal-adjustments-q3]
-                                       4 [:inputs/seasonal-adjustments-q4]
-                                       0)
-            :electricity-generation '(* [:inputs/year1-p50-yield]
-                                        [:inputs/availability]
-                                        [:compound-degradation]
-                                        [:seasonality-adjustment])
-            :revenue-from-generation '(* 1000
-                                         [:electricity-generation]
-                                         [:inputs/power-tariff])})
+  #:revenue
+   {:compound-degradation         '(when-flag [:flags/operating-period]
+                                              (/ 1 (Math/pow (inc [:inputs/annual-degradation])
+                                                             (dec [:time.contract-year/number]))))
+    :seasonality-adjustment       '(case [:time.contract-year/quarter-number]
+                                     1 [:inputs/seasonal-adjustments-q1]
+                                     2 [:inputs/seasonal-adjustments-q2]
+                                     3 [:inputs/seasonal-adjustments-q3]
+                                     4 [:inputs/seasonal-adjustments-q4]
+                                     0)
+    :electricity-generation       '(* [:inputs/year1-p50-yield]
+                                      [:inputs/availability]
+                                      [:compound-degradation]
+                                      [:seasonality-adjustment])
+    :revenue-from-generation      '(* 1000
+                                      [:electricity-generation]
+                                      [:inputs/power-tariff])})
 
 (def costs
-  #:om-costs{; TODO: Return when escalation factor done
-             ; TODOL Put this on FS
-             :escalation-factor     [:placeholder 1]
-             :expense               '(when-flag [:flags/operating-period]
-                                                (* [:escalation-factor]
-                                                   (/ [:inputs/annual-real-om-cost]
-                                                      [:inputs/periods-in-year])))})
+  #:om-costs
+   {; TODO: Return when escalation factor done
+    :escalation-factor     [:placeholder 1]
+    :expense               '(when-flag
+                             [:flags/operating-period]
+                             (* [:escalation-factor]
+                                (/ [:inputs/annual-real-om-cost]
+                                   [:inputs/periods-in-year])))})
 
 (def depreciaion
-  (merge #:depreciation{:starting-value            '(when-flag [:flags/financial-close-period]
-                                                               [:inputs/asset-value-at-start])
-                        :end-of-useful-life        '(end-of-month [:inputs/aquisition-date]
-                                                                  (* 12 [:inputs/useful-life-of-asset]))
-                        :in-useful-life-flag       '(and (date> [:time.period/start-date]
-                                                                [:inputs/aquisition-date])
-                                                         (date<= [:time.period/end-date]
-                                                                 [:end-of-useful-life]))
-                        :solar-asset-depreciation  '(when-flag [:in-useful-life-flag]
-                                                               (/ [:inputs/asset-value-at-start]
-                                                                  [:inputs/useful-life-of-asset]
-                                                                  [:inputs/periods-in-year]))}
-         (fw/corkscrew :asset-value
-                       [:depreciation/starting-value]
-                       [:depreciation/solar-asset-depreciation])))
+  (merge
+   #:depreciation
+    {:starting-value            '(when-flag
+                                  [:flags/financial-close-period]
+                                  [:inputs/asset-value-at-start])
+
+     :end-of-useful-life        '(end-of-month
+                                  [:inputs/aquisition-date]
+                                  (* 12 [:inputs/useful-life-of-asset]))
+
+     :in-useful-life-flag       '(and (date> [:time.period/start-date]
+                                             [:inputs/aquisition-date])
+                                      (date<= [:time.period/end-date]
+                                              [:end-of-useful-life]))
+
+     :solar-asset-depreciation  '(when-flag
+                                  [:in-useful-life-flag]
+                                  (/ [:inputs/asset-value-at-start]
+                                     [:inputs/useful-life-of-asset]
+                                     [:inputs/periods-in-year]))}
+
+   (fw/corkscrew :asset-value
+                 [:depreciation/starting-value]
+                 [:depreciation/solar-asset-depreciation])))
 
 
 
 (def fs
-  (merge (fw/add-total :cash-available-for-dividends
-                       #:fs.cashflow{:cash-from-invoices [:revenue/revenue-from-generation]
-                                     :om-expense-paid    '(- [:om-costs/expense])
-                                     :share-capital-redemptions '(- [:equity.share-capital/redemption])})
+  (merge (fw/add-total
+          :cash-available-for-dividends
+          #:fs.cashflow
+           {:cash-from-invoices        [:revenue/revenue-from-generation]
+            :om-expense-paid           '(- [:om-costs/expense])
+            :share-capital-redemptions '(- [:equity.share-capital/redemption])})
 
-         #:fs.cashflow{:dividends-paid '(- [:equity.dividends/dividend-paid])
-                       :net-cashflow   '(+ [:cash-available-for-dividends] [:dividends-paid])}
+         #:fs.cashflow
+          {:dividends-paid             '(- [:equity.dividends/dividend-paid])
+           :net-cashflow               '(+ [:cash-available-for-dividends] [:dividends-paid])}
 
-         (fw/add-total :profit-after-tax #:fs.income{:revenue       [:revenue/revenue-from-generation]
-                                                     :om-expense    '(- [:om-costs/expense])
-                                                     :depreciation '(- [:depreciation/solar-asset-depreciation])})
+         (fw/add-total
+          :profit-after-tax
+          #:fs.income
+           {:revenue                   [:revenue/revenue-from-generation]
+            :om-expense                '(- [:om-costs/expense])
+            :depreciation              '(- [:depreciation/solar-asset-depreciation])})
 
-         #:fs.income{:dividends-paid '(- [:equity.dividends/dividend-paid])
-                     :net-income     '(+ [:profit-after-tax] [:dividends-paid])}
+         #:fs.income
+          {:dividends-paid             '(- [:equity.dividends/dividend-paid])
+           :net-income                 '(+ [:profit-after-tax] [:dividends-paid])}
 
-         (fw/add-total :total-assets #:fs.balance-sheet.assets{:retained-cash [:equity.retained-cash/end]
-                                                               :accounts-receivable [:placeholder 0]
-                                                               :solar-asset-value [:asset-value/end]})
-         (fw/add-total :total-liabilities #:fs.balance-sheet.liabilities{:retained-earnings [:equity.retained-earnings/end]
-                                                                         :share-capital     [:equity.share-capital.balance/end]})
-         {:fs.balance-sheet/balance-check '(- [:fs.balance-sheet.assets/total-assets] [:fs.balance-sheet.liabilities/total-liabilities])}))
+         (fw/add-total
+          :total-assets
+          #:fs.balance-sheet.assets
+           {:retained-cash             [:equity.retained-cash/end]
+            :accounts-receivable       [:placeholder 0]
+            :solar-asset-value         [:asset-value/end]})
+
+         (fw/add-total
+          :total-liabilities
+          #:fs.balance-sheet.liabilities
+           {:retained-earnings         [:equity.retained-earnings/end]
+            :share-capital             [:equity.share-capital.balance/end]})
+
+         #:fs.balance-sheet
+          {:balance-check              '(- [:fs.balance-sheet.assets/total-assets]
+                                           [:fs.balance-sheet.liabilities/total-liabilities])}))
 
 (def equity
-  (merge (fw/corkscrew "equity.retained-earnings"
-                       [:fs.income/profit-after-tax]
-                       [:equity.dividends/dividend-paid])
+  (merge (fw/corkscrew
+          "equity.retained-earnings"
+          [:fs.income/profit-after-tax]
+          [:equity.dividends/dividend-paid])
 
-         (fw/corkscrew "equity.retained-cash"
-                       [:fs.cashflow/cash-available-for-dividends]
-                       [:equity.dividends/dividend-paid])
+         (fw/corkscrew
+          "equity.retained-cash"
+          [:fs.cashflow/cash-available-for-dividends]
+          [:equity.dividends/dividend-paid])
 
-         #:equity.share-capital{:drawdown '(when-flag [:flags/financial-close-period] [:inputs/asset-value-at-start])
-                                :redemption '(when-flag [:flags/financial-exit-period] [:inputs/asset-value-at-start])}
-         (fw/corkscrew "equity.share-capital.balance"
-                       [:equity.share-capital/drawdown]
-                       [:equity.share-capital/redemption])
+         #:equity.share-capital
+          {:drawdown            '(when-flag
+                                  [:flags/financial-close-period]
+                                  [:inputs/asset-value-at-start])
 
-         #:equity.dividends{:earnings-available '(max (+ [:equity.retained-earnings/end :prev]
-                                                         [:equity.retained-earnings/increase])
-                                                      0)
-                            :cash-available     '(+ [:equity.retained-cash/end :prev]
-                                                    [:equity.retained-cash/increase])
-                            :dividend-paid      '(min [:earnings-available] [:cash-available])}))
+           :redemption          '(when-flag
+                                  [:flags/financial-exit-period]
+                                  [:inputs/asset-value-at-start])}
+
+         (fw/corkscrew
+          "equity.share-capital.balance"
+          [:equity.share-capital/drawdown]
+          [:equity.share-capital/redemption])
+
+         #:equity.dividends
+          {:earnings-available  '(max (+ [:equity.retained-earnings/end :prev]
+                                         [:equity.retained-earnings/increase])
+                                      0)
+           :cash-available      '(+ [:equity.retained-cash/end :prev]
+                                    [:equity.retained-cash/increase])
+           :dividend-paid       '(min [:earnings-available] [:cash-available])}))
 
 ;; Build and run
 
