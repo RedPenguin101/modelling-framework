fe8f8b37b8a1f24f44d93653a497fdbb4d54518c
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-05-29 10:53:54 +0100

    power-up: model contracts with row-literals

--- src/models/power_up.clj
+++ src/models/power_up.clj
@@ -2,22 +2,50 @@
   (:require [fmwk.framework :as fw]
             [fmwk.utils :refer :all]))
 
-(def contracts)
+(def contract-activity
+  [:moonshine :titan :evergreen])
+
+(def contracts
+  {:moonshine {:materials  2890976
+               :salaries   3395956
+               :ovh-profit 1304539
+               :total      7591471
+               :completion [0 0 0 0 0 0 0 0.1 0.2 0.25 0.25 0.2 0]}
+   :titan     {:materials 8436341
+               :salaries 8916972
+               :ovh-profit 3600813
+               :total 20954126
+               :completion [0 0	0.05	0.1	0.15	0.2	0.2	0.2	0.1	0	0	0	0]}
+   :evergreen {:materials 7078921
+               :salaries 6821887
+               :ovh-profit 2884417
+               :total 16785225
+               :completion [0 0	0	0	0.05	0.1	0.15	0.2	0.2	0.2	0.1	0	0]}})
+
+(defn contract-input [[contract-name contract]]
+  (into (array-map)
+        (let [qualifier (str "inputs.contracts." (name contract-name))]
+          [[(keyword qualifier "completion") (into [:row-literal] (:completion contract))]
+           [(keyword qualifier "revenue") (:total contract)]
+           [(keyword qualifier "advance-month") (- 12 (count (drop-while zero? (:completion contract))))]])))
 
 (def inputs
-  #:inputs
-   {:model-start-date "2021-01-01"
-    :aquisition-date "2020-12-31"
-    :sale-date "2035-12-31"
-    :length-of-operating-period 1
-    :overhead-per-month 500000
-    :advance-payment 0.1
-    :holdback 0.1
-    :holdback-release-amount 2500000
-    :holdback-release-date "2021-06-30"
-    :existing-ppe-depreciation 3 ;years
-    :new-ppe-depreciation      5 ;years
-    })
+  (merge
+   (apply merge (map contract-input contracts))
+   #:inputs
+    {:model-start-date "2021-01-01"
+     :aquisition-date "2020-12-31"
+     :sale-date "2035-12-31"
+     :length-of-operating-period 1
+     :overhead-per-month 500000
+     :advance-payment 0.1
+     :holdback 0.1
+     :invoice-percent 0.8
+     :holdback-release-amount 2500000
+     :holdback-release-date "2021-06-30"
+     :existing-ppe-depreciation 3 ;years
+     :new-ppe-depreciation      5 ;years
+     }))
 
 (def periods
   #:period
@@ -31,10 +59,45 @@
     :number                   '(inc [:number :prev])
     :first-model-column       '(= 1 [:number])})
 
+(def contract-revenue
+  (fw/add-total
+   :total
+   (into (array-map)
+         (for [c contract-activity]
+           (let [input-qualifier (str "inputs.contracts." (name c))]
+             [(keyword "contracts.revenue" (name c))
+              (list '* [(keyword input-qualifier "revenue")]
+                    [(keyword input-qualifier "completion")])])))))
+
+(def contract-advances
+  (fw/add-total
+   :total
+   (into (array-map)
+         (for [c contract-activity]
+           (let [input-qualifier (str "inputs.contracts." (name c))]
+             [(keyword "contracts.advances" (name c))
+              (list 'if (list '= [(keyword input-qualifier "advance-month")]
+                              '(month-of [:period/end-date]))
+                    (list '* [:inputs/advance-payment]
+                          [(keyword input-qualifier "revenue")])
+                    0)])))))
+
+(def contract-accounting
+  #:contracts.accounting
+   {:revenue         [:contracts.revenue/total]
+    :advance-release '(* [:revenue]
+                         [:inputs/advance-payment])
+    :holdback-accrual '(* [:revenue]
+                          [:inputs/holdback])
+    :accounts-receivable '(* [:revenue]
+                             [:inputs/invoice-percent])
+    :acc-bal-check '(- [:revenue] [:advance-release] [:holdback-accrual] [:accounts-receivable])
+    :cash-from-invoices [:accounts-receivable :prev]})
+
 (def ebitda
   (fw/add-total :EBITDA
                 #:income.EBITDA
-                 {:revenues [:placeholder 0]
+                 {:revenues [:contracts.accounting/revenue]
                   :materials [:placeholder 0]
                   :labor [:placeholder 0]
                   :overhead [:placeholder 0]}))
@@ -47,65 +110,43 @@
                   :interest [:placeholder 0]}))
 
 (def bs-assets
-  (fw/add-total :total-assets
-                #:balance-sheet.assets
-                 {:cash [:placeholder 0]
-                  :inventory [:placeholder 0]
-                  :accounts-recievable [:placeholder 0]
-                  :holdbacks [:placeholder 5000000]
-                  :ppe [:placeholder 3600000]}))
+  (fw/add-total
+   :total-assets
+   #:balance-sheet.assets
+    {:cash '(+ [:cash :prev] [:cashflows/from-operations])
+     :inventory [:placeholder 0]
+     :accounts-receivable '(+ [:accounts-receivable :prev]
+                              [:contracts.accounting/accounts-receivable]
+                              (- [:contracts.accounting/cash-from-invoices]))
+     :holdbacks '(if [:period/first-model-column]
+                   [:placeholder 5000000]
+                   (+ [:holdbacks :prev]
+                      [:contracts.accounting/holdback-accrual]
+                      (- [:cashflows/holdbacks])))
+     :ppe [:placeholder 3600000]}))
 
 (def bs-liabs
   (fw/add-total
    :total-liabilities
    #:balance-sheet.liabilities
     {:accounts-payable [:placeholder 0]
-     :advances [:placeholder 0]
+     :advances '(+ [:advances :prev]
+                   [:contracts.advances/total]
+                   (- [:contracts.accounting/advance-release]))
      :rcf [:placeholder 2500000]
      :leasing [:placeholder 0]
      :share-capital [:placeholder 1000000]
-     :retained-earnings [:placeholder 5100000]}))
-
-(def contract-cfs
-  ;; Materials	Salaries	OVH & Profit
-  [[2890976  3395956  1304539]
-   [8436341  8916972  3600813]
-   [7078921  6821887  2884417]
-   [9543584  12290519 4530576]
-   [10637867 11845677 4665336]
-   [16935098 19125335 7482540]
-   [4193313  5262673  1962117]
-   [9373739  5521378  3090737]
-   [6011073  7906117  2887817]
-   [3928424  3068321  1451825]])
-
-(def contract-totals
-  (mapv #(apply + %) contract-cfs))
-
-(def contract-completions
-  [[0.00 0.00 0.00 0.00 0.00 0.00 0.10 0.20 0.25 0.25 0.20 0.00]
-   [0.00 0.05 0.10 0.15 0.20 0.20 0.20 0.10 0.00 0.00 0.00 0.00]
-   [0.00 0.00 0.00 0.05 0.10 0.15 0.20 0.20 0.20 0.10 0.00 0.00]
-   [0.00 0.00 0.00 0.00 0.05 0.10 0.15 0.20 0.20 0.20 0.10 0.00]
-   [0.00 0.00 0.05 0.10 0.15 0.20 0.20 0.20 0.10 0.00 0.00 0.00]
-   [0.00 0.05 0.08 0.10 0.15 0.15 0.15 0.15 0.15 0.02 0.00 0.00]
-   [0.00 0.00 0.00 0.00 0.00 0.00 0.10 0.20 0.25 0.25 0.20 0.00]
-   [0.00 0.00 0.00 0.05 0.10 0.15 0.20 0.20 0.20 0.10 0.00 0.00]
-   [0.00 0.00 0.00 0.00 0.05 0.10 0.15 0.20 0.20 0.20 0.10 0.00]
-   [0.00 0.10 0.20 0.25 0.25 0.20 0.00 0.00 0.00 0.00 0.00 0.00]])
-
-(def contract-advance-months
-  (mapv #(- 12 (count (drop-while zero? %))) contract-completions))
-
-(def contract-revenues
-  (mapv (fn [total compl] (mapv (partial * total) compl)) contract-totals contract-completions))
+     :retained-earnings '(if [:period/first-model-column]
+                           [:placeholder 5100000]
+                           (+ [:retained-earnings :prev]
+                              [:income/net-profit]))}))
 
 (def cashflow
   (fw/add-total
    :from-operations
    #:cashflows
-    {:advances [:placeholder 0]
-     :contract-revenue [:placeholder 0]
+    {:advances [:contracts.advances/total]
+     :contract-revenue [:contracts.accounting/cash-from-invoices]
      :holdbacks '(if (date= [:inputs/holdback-release-date]
                             [:period/end-date])
                    [:inputs/holdback-release-amount]
@@ -117,10 +158,15 @@
 (def bs-meta (fw/add-meta (merge bs-assets bs-liabs) {:units :currency-thousands}))
 
 (def calcs [periods bs-assets bs-liabs bs-check
-            ebitda net-profit
-            cashflow])
+            ebitda net-profit cashflow
+            contract-revenue contract-advances contract-accounting])
 (fw/fail-catch (fw/build-model2 inputs calcs))
+
 (def model (fw/build-model2 inputs calcs [bs-meta]))
+
 (def header :period/end-date)
 (def results (fw/run-model model 20))
+
+(fw/print-category results (:meta model) header "balance-sheet" 1 13)
 (fw/print-category results (:meta model) header "cashflows" 1 13)
+(fw/print-category results (:meta model) header "income" 1 13)
