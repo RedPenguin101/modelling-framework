34613f39316ba5bfd5a1008348be5ebd83798ffb
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-05-29 18:39:41 +0100

    add financial statements to template

--- src/models/template.clj
+++ src/models/template.clj
@@ -4,10 +4,13 @@
 
 (def inputs
   #:inputs
-   {:model-start-date "2020-01-01"
-    :aquisition-date "2020-12-31"
-    :sale-date "2035-12-31"
+   {:model-start-date           "2020-01-01"
+    :aquisition-date            "2020-12-31"
+    :sale-date                  "2035-12-31"
     :length-of-operating-period 12})
+
+;; TIME
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def periods
   #:period
@@ -42,9 +45,71 @@
     :last-flag         '(date= [:end]
                                [:period/end-date])})
 
-(def calcs [periods op-period])
-(fw/fail-catch (fw/build-model2 inputs calcs))
-(def model (fw/build-model2 inputs calcs))
+
+;; FINANCIAL STATEMENTS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def income
+  #:income
+   {:revenues           [:placeholder 0]
+    :operating-expenses [:placeholder 0]
+    :overheads          [:placeholder 0]
+    :EBITDA             '(+ [:revenues] [:operating-expenses] [:overheads])
+    :depreciation       [:placeholder 0]
+    :interest           [:placeholder 0]
+    :pbt                '(+ [:EBITDA] [:depreciation] [:interest])
+    :tax-expense        [:placeholder 0] ;; TODO put in a check for placeholders without 2nd element
+    :pat                '(+ [:pbt] [:tax-expense])})
+
+(def income-meta (fw/add-meta income {:units :currency :total true}))
+
+(def cashflow
+  (array-map
+   :cashflows/invoices                             [:placeholder 0]
+   :cashflows/operating-costs                      [:placeholder 0]
+   :cashflows/tax-paid                             [:placeholder 0]
+   :cashflows/cashflow-available-for-debt-service  '(+ [:invoices] [:operating-costs] [:tax-paid])
+   :cashflows/interest-paid                        [:placeholder 0]
+   :cashflows/debt-facility-drawdown               [:placeholder 0]
+   :cashflows/cashflow-available-for-equity        '(+ [:cashflow-available-for-debt-service]
+                                                       [:interest-paid]
+                                                       [:debt-facility-drawdown])
+   :cashflows/dividends-paid                       [:placeholder 0]
+   :cashflows/net-cashflow                         '(+ [:cashflow-available-for-equity]
+                                                       [:dividends-paid])))
+
+(def cashflow-meta (fw/add-meta cashflow {:units :currency :total true}))
+
+(def bs-assets
+  (fw/add-total
+   :TOTAL-ASSETS
+   #:balance-sheet.assets
+    {:cash                [:placeholder 0]
+     :accounts-receivable [:placeholder 0]
+     :non-current-assets  [:placeholder 0]}))
+
+(def bs-liabs
+  (fw/add-total
+   :TOTAL-LIABILITIES
+   #:balance-sheet.liabilities
+    {:accounts-payable  [:placeholder 0]
+     :debt              [:placeholder 0]
+     :share-capital     [:placeholder 0]
+     :retained-earnings [:placeholder 0]}))
+
+(def bs-check #:balance-sheet.checks
+               {:balance '(- [:balance-sheet.assets/TOTAL-ASSETS]
+                             [:balance-sheet.liabilities/TOTAL-LIABILITIES])})
+
+(def bs-meta (fw/add-meta (merge bs-assets bs-liabs) {:units :currency}))
+
+(def calcs [periods op-period income cashflow bs-assets bs-liabs bs-check])
+(def metadata [income-meta cashflow-meta bs-meta])
+
+(fw/fail-catch (fw/build-model2 inputs calcs metadata))
+(def model (fw/build-model2 inputs calcs metadata))
 (def header :period/end-date)
-(def results (fw/run-model model 20))
-(fw/print-category results (:meta model) header "period" 1 10)
+
+(def results (fw/run-model model 10))
+
+(fw/print-category results (:meta model) header "balance-sheet" 1 10)
