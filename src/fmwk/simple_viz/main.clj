524a340e5fe104e0a3d43161fc80feec952d168c
RedPenguin101 <allaboutthatmace1789@gmail.com>
2022-06-11 12:34:11 +0100

    simple viz better scaling

--- src/fmwk/simple_viz/main.clj
+++ src/fmwk/simple_viz/main.clj
@@ -17,12 +17,6 @@
    of 0 would mean the cross is right at the bottom of the plot
    (implying you're showing no negative values.)"
   [canvas h-size v-size v-zero]
-  (def debug {:v-size v-size
-              :v-zero v-zero
-              :zero-scaled (* v-zero v-size)
-              :old-v (int (* 0.9 v-size))
-              :new-v (int (* v-size (- 1 v-zero)))
-              :vert-ax [(int (* 0.1 h-size)) (int (* 0.9 h-size))]})
   (let [margin 0.1
         v-span (- v-size (* 2 margin v-size))
         x-cross (int (+ (* (- 1 v-zero) v-span) (* margin v-size)))]
@@ -45,11 +39,13 @@
       (c2d/line c v1 v2))))
 
 (defn scale-factors
+  "Given a series of values, will return a tuple of [constant factor].
+   These number are those, when applied, will translate the values to
+   new values of between 0 and the provided scale."
   ([values] (scale-factors values 1))
   ([values scale-adjust]
-   (let [mx (apply max values)]
-     [(- (apply min values))
-      (* scale-adjust (if (zero? mx) 1 (/ 1 (apply max values))))])))
+   (let [mn (max (- (apply min values)) 0)]
+     [mn (* scale-adjust (/ 1 (+ mn (apply max values))))])))
 
 (defn scale [values [cst fct]]
   (map #(float (* fct (+ cst %))) values))
@@ -66,10 +62,13 @@
 (def colors [:red :blue :green :orange])
 
 (defn series-lines-save [series filename]
-  (let [canvas (c2d/canvas 1000 1000)
-        y-scale (scale-factors (apply concat series) 800)
-        x-vals (map #(+ 100 %) (calc-and-scale (range 0 (apply max (map count series))) 800))]
-    (draw-axis-lines canvas 1000 1000)
+  (let [canvas   (c2d/canvas 1000 1000)
+        c-series (apply concat series)
+        y-scale  (scale-factors c-series 800)
+        y-range  (- (apply max c-series) (apply min c-series))
+        z-point  (min 1 (max 0 (/ (- (apply min c-series)) y-range)))
+        x-vals   (map #(+ 100 %) (calc-and-scale (range 0 (apply max (map count series))) 800))]
+    (draw-axis-lines2 canvas 1000 1000 z-point)
     (doseq [[s c] (map vector series colors)]
       (draw-lines canvas (lines-from-points (map vector x-vals (flip-vals (map #(+ 100 %) (scale s y-scale)))))
                   c))
@@ -80,12 +79,8 @@
         c-series (apply concat series)
         y-scale  (scale-factors c-series 800)
         y-range  (- (apply max c-series) (apply min c-series))
-        z-point  (max 0 (/ (- (apply min c-series)) y-range))
+        z-point  (min 1 (max 0 (/ (- (apply min c-series)) y-range)))
         x-vals   (map #(+ 100 %) (calc-and-scale (range 0 (apply max (map count series))) 800))]
-    (println "y-scale" y-scale)
-    (println "y-range" y-range)
-    (println "z-point" z-point)
-    (println "x-vals" x-vals)
     (draw-axis-lines2 canvas 1000 1000 z-point)
     (doseq [[s c] (map vector series colors)]
       (draw-lines canvas (lines-from-points (map vector x-vals (flip-vals (map #(+ 100 %) (scale s y-scale)))))
@@ -99,6 +94,14 @@
 
   (series-lines-save ['(0 0 0 0 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 16000)
                       '(0 0 0 0 2000 4000 6000 8000 10000 12000 14000 16000 18000 20000 22000 24000 26000 28000 30000 32000)]
-                     "graph.png"))
+                     "graph.png")
 
-(series-lines [(range -10 10)])
+  (series-lines [(range 1 10)])
+
+  (series-lines [(range 0 10)])
+
+  (series-lines [(range -1 10)])
+
+  (series-lines [(range -10 10)])
+
+  (series-lines [(range -10 0)]))
